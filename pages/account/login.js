fdc2f0708c8f287fc6ccd4eaa77471d1dd7215b4
kcinnoy <yonnick00@gmail.com>
2021-12-12 22:04:19 +0000

    login and register forms ui built

--- pages/account/login.js
+++ pages/account/login.js
@@ -3,25 +3,57 @@ import 'react-toastify/dist/ReactToastify.css';
 import { useState, useEffect, useContext } from 'react';
 import Link from 'next/link';
 import Layout from '@/components/Layout';
-import { Input, Form, Card, Icon } from 'semantic-ui-react';
+import { Input, Form, Card, Icon, Container, Grid } from 'semantic-ui-react';
+
 
 export default function LoginPage() {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        console.log({email, password})
+    };
 
     return (
         <Layout title='User Login'>
-            <Card>
+            <Card ui centered style={{marginTop: 30}}>
                 <Card.Content>
                     <Card.Header>
                         <Icon name='user circle' />
                         Login
                     </Card.Header>
+                    <ToastContainer />
                 </Card.Content>
                 <Card.Content>
-                    
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Field
+                            type='email'
+                            control={Input}
+                            id='email'
+                            //name='name'
+                            label='Email Address'
+                            placeholder='Enter emial'
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <Form.Field
+                            type='password'
+                            control={Input}
+                            id='password'
+                            //name='name'
+                            label='Password'
+                            ///placeholder=''
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <Form.Button>Login</Form.Button>
+                    </Form>
+                </Card.Content>
+                <Card.Content>
+                    Don't have an account? &nbsp; <Link href='/account/register'>Register</Link>
                 </Card.Content>
             </Card>
         </Layout>
-    )
+    );
 }
--- pages/account/register.js
+++ pages/account/register.js
@@ -1,8 +1,85 @@
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useState, useEffect, useContext } from 'react';
+import Link from 'next/link';
+import Layout from '@/components/Layout';
+import { Input, Form, Card, Icon, Container, Grid } from 'semantic-ui-react';
+import { auto } from '@popperjs/core';
 
 export default function RegisterPage() {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [passwordConfirm, setPasswordConfirm] = useState('');
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        if (password !== passwordConfirm) {
+            toast.error('Passwords do not match!')
+        }
+        console.log({username, email, password})
+    };
+
     return (
-        <div>
-            register
-        </div>
-    )
+        <Layout title='User Registration'>
+            <Card ui centered style={{marginTop: 30}}>
+                <Card.Content>
+                    <Card.Header>
+                        <Icon name='user circle' />
+                        Register
+                    </Card.Header>
+                    <ToastContainer />
+                </Card.Content>
+                <Card.Content>
+                    <Form onSubmit={handleSubmit}>
+                    <Form.Field
+                            type='text'
+                            control={Input}
+                            id='username'
+                            //name='name'
+                            label='Username'
+                            //placeholder=''
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                        <Form.Field
+                            type='email'
+                            control={Input}
+                            id='email'
+                            //name='name'
+                            label='Email Address'
+                            placeholder='Enter emial'
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <Form.Field
+                            type='password'
+                            control={Input}
+                            id='password'
+                            //name='name'
+                            label='Password'
+                            ///placeholder=''
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                         <Form.Field
+                            type='password'
+                            control={Input}
+                            id='passwordConfirm'
+                            //name='name'
+                            label='Confirm Password'
+                            ///placeholder=''
+                            value={passwordConfirm}
+                            onChange={(e) => setPasswordConfirm(e.target.value)}
+                        />
+                        <Form.Button>Register</Form.Button>
+                    </Form>
+                </Card.Content>
+                <Card.Content>
+                    Already have an account? &nbsp; <Link href='/account/login'>Login</Link>
+                </Card.Content>
+            </Card>
+        </Layout>
+    );
 }
